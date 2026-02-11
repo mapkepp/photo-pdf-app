92f5b20a14d7964e3b3c871009a1a0b6f5597c9d
mapkepp <71609476+mapkepp@users.noreply.github.com>
2026-02-11 21:00:20 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { initEventListeners } from './event-listeners.js';
-import { renderPhotos } from './photo-display.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-    initEventListeners();
-    // Инициализируем глобальный массив фото
-    window.photos = [];
-});
