219cda870a193a9c259b8c3e68103496aef9772d
mapkepp <71609476+mapkepp@users.noreply.github.com>
2026-02-11 19:54:17 +0300

    Update photo-utils.js

--- photo-utils.js
+++ photo-utils.js
@@ -1,11 +1,13 @@
-// photo-utils.js
 export function updatePhotoOrders() {
     if (!window.photos) {
-        console.warn('Массив window.photos не инициализирован, пропускаем обновление порядков');
+        console.warn('Массив window.photos не инициализирован');
         return;
     }
-
     window.photos.forEach((photo, index) => {
         photo.order = index;
     });
+}
+
+export function clearPhotos() {
+    window.photos = [];
 }
