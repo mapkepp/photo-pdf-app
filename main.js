0c5bdb6a7d377fa3960345dad2c6a5e147f0a938
mapkepp <71609476+mapkepp@users.noreply.github.com>
2026-02-11 19:33:39 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
 import { initEventListeners } from './event-listeners.js';
 import { renderPhotos } from './photo-display.js';
 
-import { generatePdf } from './pdf-generator.js';
-
-
-// Глобальные переменные
-window.photos = [];
-const photoContainer = document.getElementById('photo-container');
-const titleInput = document.getElementById('title');
-const uploadInput = document.getElementById('photo-upload');
-const generatePdfBtn = document.getElementById('generate-pdf');
-const downloadLink = document.getElementById('download-link');
-
-// Инициализация приложения
 document.addEventListener('DOMContentLoaded', function() {
-    initEventListeners({
-        photoContainer,
-        titleInput,
-        uploadInput,
-        generatePdfBtn,
-        downloadLink
-    });
+    initEventListeners();
+    // Инициализируем глобальный массив фото
+    window.photos = [];
 });
