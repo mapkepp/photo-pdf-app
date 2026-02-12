1dfd818aa89065f95d23b95911251d26eeeb6cb5
mapkepp <71609476+mapkepp@users.noreply.github.com>
2026-02-12 12:06:56 +0300

    Delete pdf-page-manager.js

--- pdf-page-manager.js
+++
@@ -1,7 +0,0 @@
-export function checkAndAddNewPage(doc, currentY) {
-    if (currentY > 280) {
-        doc.addPage();
-        return 20; // новая позиция Y на новой странице
-    }
-    return currentY;
-}
